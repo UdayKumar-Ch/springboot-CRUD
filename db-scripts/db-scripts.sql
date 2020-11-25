6b66ff014e535ae799ef6f655c49aa1e6a0a0fbe
UdayKumar-Ch <56138653+UdayKumar-Ch@users.noreply.github.com>
2020-11-25 21:26:23 +0000

    Revert "Changing app to deploy with a docker compose functionality"
    This reverts commit 44f2b3be90ac7680c7500270b652a9e4ce5c05c5.

--- db-scripts/db-scripts.sql
+++ db-scripts/db-scripts.sql
@@ -15,7 +15,7 @@ CREATE SEQUENCE address_id_seq INCREMENT BY 1 START 1001;
 
 create table address(
 address_id INT NOT NULL DEFAULT NEXTVAL('address_id_seq'),
-email_id varchar(20) references customers(email_id),
+email_id varchar(10) references customers(email_id),
 line1 varchar(20) not null,
 line2 varchar(20) ,
 line3 varchar(20) ,
@@ -24,18 +24,9 @@ postal_code varchar(20) not null,
 country varchar(20) not null,
 state varchar(20) not null);
 
---create table customer_address(
---email_id varchar(20),
---address_id INT ,
---primary key (email_id, address_id),
---constraint fk_addr_email
---	foreign key (address_id,);
+
 
 
 -- CREATE USER postgres SUPERUSER;
 -- CREATE DATABASE postgres WITH OWNER postgres;
-
--- psql -U username -d myDataBase -a -f myInsertFile
--- https://medium.com/@aeadedoyin/getting-started-with-postgresql-on-windows-201906131300-ee75f066df78
-
 
